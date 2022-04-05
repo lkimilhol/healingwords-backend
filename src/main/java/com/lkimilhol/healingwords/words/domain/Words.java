ccc876226944b93a34cc25983a5d18c3abb55bf4
lkimilhol <lkimilhol@gmail.com>
2022-04-05 18:09:18 +0900

    개발 내용 추가

--- src/main/java/com/lkimilhol/healingwords/words/domain/Words.java
+++ src/main/java/com/lkimilhol/healingwords/words/domain/Words.java
@@ -12,5 +12,14 @@ public class Words {
     @Id
     private Long id;
 
+    // TODO 포장 클래스 생성
+    private String content;
 
+    // TODO 글쓴이 작성
+
+    // TODO 비밀번호 작성
+
+    // TODO 작성일시
+
+    // TODO 수정일시
 }
