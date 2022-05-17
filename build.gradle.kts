15f81a3a0aedfc9cd4a4ab532ef9c23cb2b3f1ca
lkimilhol <lkimilhol@gmail.com>
2022-05-17 23:33:34 +0900

    mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
+	testImplementation("io.mockk:mockk:1.12.0")
 }
 
 noArg {
