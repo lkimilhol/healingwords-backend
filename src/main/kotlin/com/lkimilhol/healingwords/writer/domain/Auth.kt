c9e01a945c1a56bfe44a6e4f4686b12be9ad70e5
lkimilhol <lkimilhol@gmail.com>
2022-11-12 18:35:49 +0900

    feature: writer 도메인 재정의

---
+++ src/main/kotlin/com/lkimilhol/healingwords/writer/domain/Auth.kt
@@ -0,0 +1,7 @@
+package com.lkimilhol.healingwords.writer.domain
+
+enum class Auth(description: String) {
+    ADMIN("관리자"),
+    MEMBER("회원")
+    ;
+}
--- src/main/kotlin/com/lkimilhol/healingwords/writer/domain/Writer.kt
+++ src/main/kotlin/com/lkimilhol/healingwords/writer/domain/Writer.kt
@@ -1,35 +1,28 @@
 package com.lkimilhol.healingwords.writer.domain
 
-import javax.persistence.Embeddable
-import javax.persistence.Embedded
+import javax.persistence.*
 
-@Embeddable
-class Writer private constructor(
+@Entity
+class Writer(
     @Embedded
-    private var name: Name,
-
-    @Embedded
-    private var password: Password,
+    private var name: Name?,
 
     @Embedded
-    private var email: Email
+    private var password: Password?,
+
+    @Embedded
+    private var email: Email?,
+
+    @Enumerated(value = EnumType.STRING)
+    private var auth: Auth
 ) {
-
-    fun name(): String {
-        return name.content()
-    }
-
-    fun password(): String {
-        return password.contents()
-    }
-
-    fun email(): String {
-        return email.contents()
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long? = null
 
     companion object {
-        fun create(name: Name, password: Password, email: Email): Writer {
-            return Writer(name, password, email)
+        fun create(name: Name, password: Password, email: Email, auth: Auth): Writer {
+            return Writer(name, password, email, auth)
         }
     }
 }
