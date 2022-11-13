0f3074ddeb40116845e19dc3093b94245a9716d9
lkimilhol <lkimilhol@gmail.com>
2022-11-13 10:37:20 +0900

    refactor: 테스트케이스 에러 수정

--- src/main/kotlin/com/lkimilhol/healingwords/words/domain/Text.kt
+++ src/main/kotlin/com/lkimilhol/healingwords/words/domain/Text.kt
@@ -5,12 +5,22 @@ import javax.persistence.Column
 import javax.persistence.Embeddable
 
 @Embeddable
-class Text {
+class Text(
     @Column(name = "text", nullable = false)
-    private var content: String = ""
+    private var content: String
+) {
+
+    fun content(): String {
+        return content
+    }
 
     companion object {
         const val MAX_LENGTH = 255
+
+        fun create(content: String): Text {
+            checkLength(content)
+            return Text(content)
+        }
 
         private fun checkLength(content: String) {
             if (content.length > MAX_LENGTH) {
