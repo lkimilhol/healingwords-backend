7e785a7a864ddb639bb4df950806fc6db5e57832
lkimilhol <lkimilhol@gmail.com>
2022-11-15 23:41:48 +0900

    feature: 테스트케이스 추가

--- src/test/kotlin/com/lkimilhol/healingwords/writer/service/WriterServiceTest.kt
+++ src/test/kotlin/com/lkimilhol/healingwords/writer/service/WriterServiceTest.kt
@@ -1,35 +1,58 @@
-//package com.lkimilhol.healingwords.writer.service
-//
-//import com.lkimilhol.healingwords.writer.domain.Email
-//import com.lkimilhol.healingwords.writer.domain.Name
-//import com.lkimilhol.healingwords.writer.domain.Password
-//import com.lkimilhol.healingwords.writer.domain.Writer
-//import io.mockk.every
-//import io.mockk.mockk
-//import org.assertj.core.api.Assertions.assertThat
-//import org.junit.jupiter.api.Test
-//import org.mockito.ArgumentMatchers.anyLong
-//import java.util.*
-//
-//internal class WriterServiceTest {
-//    private val writerRepository = mockk<WriterRepository>()
-//    private val writerService = WriterService(writerRepository)
-//
-//    @Test
-//    fun `작가 정보`() {
-//        // given
-//        val writer = Writer.create(
-//            Name.create("name"),
-//            Password.create("password"),
-//            Email.create("test@healingwords.com"),
-//        )
-//
-//        // when
-//        every { writerRepository.findById(anyLong()) } returns Optional.of(writer)
-//        val writerInfo = writerService.get(anyLong())
-//
-//        // then
-//        assertThat(writerInfo.name).isEqualTo("name")
-//        assertThat(writerInfo.email).isEqualTo("test@healingwords.com")
-//    }
-//}
+package com.lkimilhol.healingwords.writer.service
+
+import com.lkimilhol.healingwords.writer.dto.MemberAddDto
+import com.lkimilhol.healingwords.writer.exception.AlreadyMemberException
+import com.lkimilhol.healingwords.writer.exception.DuplicateNicknameException
+import com.lkimilhol.healingwords.writer.repository.WriterRepository
+import io.mockk.Called
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+internal class WriterServiceTest {
+    private val writerRepository = mockk<WriterRepository>()
+    private val writerService = WriterService(writerRepository)
+
+    @Test
+    fun `이메일이 중복되면 회원 가입이 불가능하다`() {
+        // given
+        val memberAddDto = MemberAddDto(
+            nickname = "nickname",
+            password = "password",
+            email = "test@test.com",
+        )
+
+        // when
+        every { writerRepository.existsByEmail(any()) } returns true
+
+        // then
+        assertThatThrownBy {
+            writerService.addMember(memberAddDto)
+        }.isInstanceOf(AlreadyMemberException::class.java)
+
+        verify {writerRepository.save(any()) wasNot Called }
+    }
+
+    @Test
+    fun `닉네임이 중복되면 회원 가입이 불가능하다`() {
+        // given
+        val memberAddDto = MemberAddDto(
+            nickname = "nickname",
+            password = "password",
+            email = "test@test.com",
+        )
+
+        // when
+        every { writerRepository.existsByEmail(any()) } returns false
+        every { writerRepository.existsByNickname(any()) } returns true
+
+        // then
+        assertThatThrownBy {
+            writerService.addMember(memberAddDto)
+        }.isInstanceOf(DuplicateNicknameException::class.java)
+
+        verify {writerRepository.save(any()) wasNot Called }
+    }
+}
