6c68bc8c79e90846640627f18ea15b1703516c23
lkimilhol <lkimilhol@gmail.com>
2022-11-15 23:24:50 +0900

    feature: 중복 유효성 검사 체크

--- src/main/kotlin/com/lkimilhol/healingwords/writer/repository/WriterRepository.kt
+++ src/main/kotlin/com/lkimilhol/healingwords/writer/repository/WriterRepository.kt
@@ -1,7 +1,11 @@
 package com.lkimilhol.healingwords.writer.repository
 
+import com.lkimilhol.healingwords.writer.domain.Email
+import com.lkimilhol.healingwords.writer.domain.Nickname
 import com.lkimilhol.healingwords.writer.domain.Writer
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface WriterRepository : JpaRepository<Writer, Long> {
+    fun existsByNickname(nickname: Nickname): Boolean
+    fun existsByEmail(email: Email): Boolean
 }
